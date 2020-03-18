9bfb05efb8dfeb63ce25df322112158f070e8d84
Szanyi Krisztián <szkript@gmail.com>
2020-03-18 17:54:01 +0100

    incrementing folder assign. base aggregator in assistant.py

--- assistant.py
+++ assistant.py
@@ -2,8 +2,16 @@ from game_components.table import Table
 
 
 class Assistant:
+    table = None
+
+    def __init__(self):
+        self.table = Table()
+
     def start(self):
-        pass
+        while True:
+            self.table.analyze()
+            if self.table.get_img_count() > 10:
+                break
 
 
 if __name__ == '__main__':
--- game_components/table.py
+++ game_components/table.py
@@ -1,9 +1,12 @@
 import pyautogui
+import os
+from os.path import join, isdir
 
 
 class Table:
-    SCREENSHOT_FOLDER = "desktop_screenshots/"
-    GATHERING_FOLDER = "image_gathering/"
+    __SCREENSHOT_FOLDER = "desktop_screenshots"
+    __GATHERING_FOLDER = "image_gathering"
+    __BASE_PATH = None
 
     hand = None
     middle = None
@@ -12,15 +15,39 @@ class Table:
     __classifier = None
 
     __img_count = 0
-    __current_file_name = ""
+    __current_file_name = None
 
     def __init__(self):
+        self.__set_folder_path()
         # create and set next folder for observe images
         # classifier init
         pass
 
+    def get_img_count(self):
+        return self.__img_count
+
+    def analyze(self):
+        self.__take_screenshot()
+
     def __take_screenshot(self):
         screenshot = pyautogui.screenshot()
-        self.__current_file_name = f'{self.SCREENSHOT_FOLDER}desktop-{self.__img_count}.jpg'
+        self.__current_file_name = f'{self.__BASE_PATH}\\desktop-{self.__img_count}.jpg'
         screenshot.save(self.__current_file_name)
         self.__img_count += 1
+
+    def __set_folder_path(self):
+        actual_path = join(os.getcwd(), self.__SCREENSHOT_FOLDER)
+        if not isdir(actual_path):
+            os.makedirs(actual_path)
+        directories = [x[0] for x in os.walk(actual_path)]
+        try:
+            next_folder = str(int(directories[-1][-1]) + 1)
+        except ValueError:
+            next_folder = str(0)
+
+        directory = actual_path + "\\" + next_folder
+        print("The current working directory is %s" % directory + "\n")
+        if not isdir(directory):
+            os.makedirs(directory)
+
+        self.__BASE_PATH = directory
