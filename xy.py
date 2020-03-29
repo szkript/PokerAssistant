c08933a0557d1f585d4fa1d1964fe2bfa0e2476d
Szanyi Krisztián <szkript@gmail.com>
2020-03-29 20:59:34 +0200

    roi save

--- xy.py
+++ xy.py
@@ -5,7 +5,7 @@ import cv2
 refPt = []
 cropping = False
 
-
+imcount = 0
 def click_and_crop(event, x, y, flags, param):
     # grab references to the global variables
     global refPt, cropping
@@ -26,40 +26,41 @@ def click_and_crop(event, x, y, flags, param):
         cv2.imshow("image", image)
 
 
-# construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True, help="Path to the image")
-# args = vars(ap.parse_args())
-# load the image, clone it, and up the mouse callback function
-impath = "image.jpg"
-image = cv2.imread(impath)
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
-# keep looping until the 'q' key is pressed
+stop_exit = False
 while True:
-    # display the image and wait for a keypress
-    cv2.imshow("image", image)
-    key = cv2.waitKey(1) & 0xFF
-    # if the 'r' key is pressed, reset the cropping region
-    if key == ord("r"):
-        image = clone.copy()
-    # if the 'e' key is pressed, break from the loop
-    elif key == ord("e"):
+    # load the image, clone it, and up the mouse callback function
+    impath = "image.jpg"
+    image = cv2.imread(impath)
+    clone = image.copy()
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", click_and_crop)
+    # keep looping until the 'q' key is pressed
+    while True:
+        # display the image and wait for a keypress
+        cv2.imshow("image", image)
+        key = cv2.waitKey(1) & 0xFF
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("r"):
+            image = clone.copy()
+        # if the 'e' key is pressed, break from the loop
+        elif key == ord("e"):
+            stop_exit = True
+            break
+        # if the 'c' key is pressed, continue extracting
+        elif key == ord("c"):
+            break
+    if stop_exit:
         break
-    # if the 'c' key is pressed, continue extracting
-    elif key == ord("c"):
-        continue
+    # if there are two reference points, then crop the region of interest
+    # from the image and display it
+    if len(refPt) == 2:
+        roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+        coordinates = f"img: {imcount}  x,y : {refPt[0]}, x2,y2: {refPt[1]}, width: {refPt[1][1] - refPt[0][1]}, height: {refPt[1][0] - refPt[0][0]}"
+        print(coordinates)
+        cv2.imwrite(f"image_gathering/roi{imcount}.jpg", roi)
+        with open(f"image_gathering/roi{imcount}.txt", "w") as coord:
+            coord.write(coordinates)
+        imcount += 1
 
-
-# if there are two reference points, then crop the region of interest
-# from teh image and display it
-if len(refPt) == 2:
-    roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-    # cv2.imshow("ROI", roi)
-    # cv2.waitKey(0)
-    print(f"x,y : {refPt[0]}, x2,y2: {refPt[1]}, width: {refPt[1][1]-refPt[0][1]}, height: {refPt[1][0] - refPt[0][0]}")
-    cv2.imwrite("roi.jpg", roi)
-
-# close all open windows
-cv2.destroyAllWindows()
+    # close all open windows
+    cv2.destroyAllWindows()
